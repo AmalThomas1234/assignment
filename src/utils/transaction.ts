0a8f853615d68311894b1df2a1c464c3910d7c3f
AmalThomas1234 <amalthomas964@gmail.com>
2024-06-13 11:49:13 +0530

    Original code

---
+++ src/utils/transaction.ts
@@ -0,0 +1,9 @@
+export interface Transaction {
+  id: string;
+  date: string;
+  amount: number;
+  description: string;
+  employeeId: string;
+  employeeName: string;
+  // Add other fields as necessary based on your data structure
+}
